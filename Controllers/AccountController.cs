24753c9c365942ec39a2052db2911ccaa6c1c780
OAK-GROUP\vargas8460 <vargas8460@oak-group.org>
2024-11-06 09:34:55 +0900

    Inicial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/Account/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("/Account/Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("/Account/Logout")]
+        public IActionResult Logout()
+        {
+            // Add logout logic
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
