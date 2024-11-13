2851259b957e8d448f73e749ec895190c55894e3
OAK-GROUP\vargas8460 <vargas8460@oak-group.org>
2024-11-13 15:35:18 +0900

    Sample Details template

---
+++ Controllers/Dashboards.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bioinformatics.Controllers
+{
+    public class Dashboards : Controller
+    {
+        public IActionResult GeneralReport()
+        {
+            return View();
+        }
+
+        public IActionResult SamplesDetails()
+        {
+            return View();
+        }
+
+        public IActionResult GenderDiscovery()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SamplesController1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bioinformatics.Controllers
+{
+	public class Samples : Controller
+	{
+		public IActionResult MinionReport()
+		{
+			string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "minion", "report.html");
+			string htmlContent = System.IO.File.ReadAllText(path);
+
+			return Content(htmlContent, "text/html");
+		}
+	}
+}
